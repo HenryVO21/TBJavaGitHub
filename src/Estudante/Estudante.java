1d3af540045a64e6623919b1c4edf24b8b0ce617
HaiwaTamashii <henryvitaloliveira13@gmail.com>
2026-08-22 00:44:10 -0300

    Estudante

--- src/Estudante/Estudante.java
+++ src/Estudante/Estudante.java
@@ -17,13 +17,15 @@ public class Estudante {
         for (int i = 0; i < 5; i ++){
             System.out.println("Digite a "+ (i+1) +"nota do aluno: ");
             notas[i] = teclado.nextDouble();
-            if (notas[i] < 0){
+
+            //queria fazer um validação do valor inserido pelo usuario para nenhum ser negativo
+            /*if (notas[i] < 0){
                 System.out.println("\nValor invalido...");
                 while (notas[i]<0){
                     System.out.println("\nDigite novamente:");
                     notas[i] = teclado.nextDouble();
                 }
-            }
+            }*/
         }
     }
     public double CalculaMedia(){
