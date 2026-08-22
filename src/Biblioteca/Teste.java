aff2f205be3d8950c5e5b938c6ea3e7a17aa5b1f
HaiwaTamashii <henryvitaloliveira13@gmail.com>
2026-08-22 15:57:51 -0300

    Biblioteca

---
+++ src/Biblioteca/Teste.java
@@ -0,0 +1,18 @@
+package Biblioteca;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        Livro lv = new Livro(true,"Divina Comédia: O Inferno de Dante");
+
+        System.out.println("Livro:");
+        System.out.println("\n\tTitulo: "+ lv.getTitulo());
+        System.out.println("\n\tStatus: "+ lv.ViewStatusBook());
+
+
+
+
+
+
+    }
+}
