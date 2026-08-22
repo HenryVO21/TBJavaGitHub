ce66d2fc32c562d0523501d614998ec65ea38fd9
HaiwaTamashii <henryvitaloliveira13@gmail.com>
2026-08-22 15:22:09 -0300

    Biblioteca

--- src/Biblioteca/Periodico.java
+++ src/Biblioteca/Periodico.java
@@ -1,4 +1,12 @@
 package Biblioteca;
 
-public class Periodico {
+public class Periodico extends Publicacao {
+
+    private int volume;
+
+    public Periodico(String titulo, int volume){
+        super(titulo);
+        this.volume=volume;
+
+    }
 }
