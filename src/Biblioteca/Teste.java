de6bc5eec298e2a730cd0fd9173f5d036e780725
HaiwaTamashii <henryvitaloliveira13@gmail.com>
2026-08-22 17:15:40 -0300

    Biblioteca

--- src/Biblioteca/Teste.java
+++ src/Biblioteca/Teste.java
@@ -11,10 +11,10 @@ public class Teste {
 
         Livro lv = new Livro(ttLivro, true);
 
-        System.out.print("Digite o nome do Periódico");
+        System.out.print("\nDigite o nome do Periódico: ");
         String ttPeriodico = sc.nextLine();
 
-        System.out.print("Digite o nome do Periódico");
+        System.out.print("Digite o numero do Volume: ");
         int volume = sc.nextInt();
 
         Periodico pr = new Periodico(ttPeriodico,volume);
