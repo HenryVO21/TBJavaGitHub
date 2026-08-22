d909dffe8e898290b85ea38142ee66ec6931931f
HaiwaTamashii <henryvitaloliveira13@gmail.com>
2026-08-22 00:24:24 -0300

    Estudante

--- src/Estudante/Estudante.java
+++ src/Estudante/Estudante.java
@@ -78,15 +78,4 @@ public class Estudante {
         }
         return alunosAprovados;
     }
-
-    public double CalculaMedia(int[] peso){
-        double somaNota = 0;
-        int quantidadeProvas = 0;
-
-        for (int i = 0 ; i < 5 ; i++){
-            somaNota += notas[i] * peso[i];
-            quantidadeProvas += peso[i];
-        }
-        return somaNota/quantidadeProvas;
-    }
 }
