7d4675e9e2bc95b1692681bb8894c7be88f1f09e
HaiwaTamashii <henryvitaloliveira13@gmail.com>
2026-08-22 17:10:13 -0300

    Biblioteca

--- src/Biblioteca/Teste.java
+++ src/Biblioteca/Teste.java
@@ -4,20 +4,31 @@ import java.util.Scanner;
 
 public class Teste {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-
+        Scanner sc = new Scanner(System.in);
+        
         System.out.print("Digite o nome do livro: ");
-        String ttLivro = teclado.nextLine();
+        String ttLivro = sc.nextLine();
 
         Livro lv = new Livro(ttLivro, true);
 
         System.out.print("Digite o nome do Periódico");
-        String ttPeriodico = teclado.nextLine();
+        String ttPeriodico = sc.nextLine();
 
         System.out.print("Digite o nome do Periódico");
-        int volume = teclado.nextInt();
+        int volume = sc.nextInt();
 
         Periodico pr = new Periodico(ttPeriodico,volume);
+
+        System.out.println("\n--- LIVRO ---");
+        System.out.println("Título: " + lv.getTitulo());
+        System.out.println("Disponível: " + lv.ViewStatusBook());
+
+        System.out.println("\n--- PERIÓDICO ---");
+        System.out.println("Título: " + pr.getTitulo());
+        System.out.println("Volume: " + pr.getVolume());
+
+        lv.Empretimo();
+
 
     }
 }
