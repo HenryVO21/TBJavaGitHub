2f9a3c9bde4b15fc6030fdecb7db51462bc5f232
HaiwaTamashii <henryvitaloliveira13@gmail.com>
2026-08-22 19:24:28 -0300

    Biblioteca

--- src/Estudante/Teste.java
+++ src/Estudante/Teste.java
@@ -24,6 +24,37 @@ public class Teste {
 
             estudantes[i].InsereNotas();
 
+            System.out.println("\nComo deseja calcular a média?");
+            System.out.println("1 - Média normal");
+            System.out.println("2 - Média ponderada");
+            System.out.print("Escolha: ");
+
+            int escolha = teclado.nextInt();
+
+            switch (escolha) {
+
+                case 1:
+                    System.out.println("Média: " + estudantes[i].CalculaMedia());
+                    break;
+
+                case 2:
+                    int[] pesos = new int[5];
+
+                    System.out.println("\n--- PESOS ---");
+
+                    for (int j = 0; j < 5; j++) {
+                        System.out.print("Digite o peso da " + (j + 1) + "ª nota: ");
+                        pesos[j] = teclado.nextInt();
+                    }
+
+                    System.out.println("Média ponderada: "
+                            + estudantes[i].CalculaMedia(pesos));
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+            }
+
             System.out.println("\nNome: " + estudantes[i].getNomeAluno());
             System.out.println("Média: " + estudantes[i].CalculaMedia());
             System.out.println("Menor nota: " + estudantes[i].MenorNota());
