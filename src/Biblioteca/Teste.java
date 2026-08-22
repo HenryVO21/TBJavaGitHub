fb6cf88fde9291e1c186c1423e9f9d582e907418
HaiwaTamashii <henryvitaloliveira13@gmail.com>
2026-08-22 16:06:16 -0300

    Biblioteca

--- src/Biblioteca/Teste.java
+++ src/Biblioteca/Teste.java
@@ -6,7 +6,10 @@ public class Teste {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
+        System.out.print("Digite o nome do livro: ");
+        String ttLivro = teclado.nextLine();
 
+        Livro lv = new Livro(ttLivro);
 
 
 
